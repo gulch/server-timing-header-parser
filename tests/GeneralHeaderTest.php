a848d890c2c60bb7702f53a48571f230606a7872
gulch <hello@gulch.uk>
2025-04-29 14:24:45 +0300

    add simple test

---
+++ tests/GeneralHeaderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace gulch\ServerTimingHeaderParser\Tests;
+
+use PHPUnit\Framework\TestCase;
+use gulch\ServerTimingHeaderParser\Processor;
+
+class GeneralHeaderTest extends TestCase
+{
+    public function testGeneralHeader()
+    {
+        $processor = new Processor();
+
+        $header = '01_bootstrap;dur=4.4,02_app;dur=17.37,total;dur=21.91';
+
+        $result = [
+            [
+                'name' => '01_bootstrap',
+                'dur' => 4.4
+            ],
+            [
+                'name' => '02_app',
+                'dur' => 17.37,
+            ],
+            [
+                'name' => 'total',
+                'dur' => 21.91,
+            ]
+        ];
+
+        $this->assertSame(
+            $result,
+            $processor->process($header)
+        );
+    }
+}
